d32cbbf9555b544ec6617d2ea8833d6d0e1a79b5
TuNombreDeUsuario <joel.diaz.lopez7@gmnail.com>
2025-06-06 11:16:52 -0600

    First commit

---
+++ src/main/java/tecdesoftware/donut_app/DonutAppApplication.java
@@ -0,0 +1,13 @@
+package tecdesoftware.donut_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonutAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonutAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tecdesoftware/donut_app/DonutAppApplicationTests.java
@@ -0,0 +1,13 @@
+package tecdesoftware.donut_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonutAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
